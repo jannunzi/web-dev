33637995ed759189900cd5e81f8f182b42d3ccd4
jannunzi <jannunzi@gmail.com>
2021-10-06 22:18:16 -0400

    replace class with className

--- src/components/WhoToFollowList/WhoToFollowListItem.js
+++ src/components/WhoToFollowList/WhoToFollowListItem.js
@@ -2,22 +2,22 @@ import React from "react";
 
 const WhoToFollowListItem = (who) => {
     return(
-        <li class="list-group-item">
-            <div class="row">
-                <div class="col-2">
-                    <img src="${who.avatarIcon}" width="48" class="rounded-circle float-start"/>
+        <li className="list-group-item">
+            <div className="row">
+                <div className="col-2">
+                    <img src="${who.avatarIcon}" width="48" className="rounded-circle float-start"/>
                 </div>
-                <div class="col-8">
-                    <div class="fw-bold">${who.userName}
-                        <span class="fa-stack" style="font-size: 0.5em">
-                          <i class="fas fa-circle fa-stack-2x"></i>
-                          <i class="fas fa-check fa-stack-1x fa-inverse"></i>
+                <div className="col-8">
+                    <div className="fw-bold">${who.userName}
+                        <span className="fa-stack" style="font-size: 0.5em">
+                          <i className="fas fa-circle fa-stack-2x"></i>
+                          <i className="fas fa-check fa-stack-1x fa-inverse"></i>
                         </span>
                     </div>
                     <div>@${who.handle}</div>
                 </div>
-                <div class="col-2">
-                    <button class="rounded-pill float-end btn btn-primary mt-1">Follow</button>
+                <div className="col-2">
+                    <button className="rounded-pill float-end btn btn-primary mt-1">Follow</button>
                 </div>
             </div>
         </li>
