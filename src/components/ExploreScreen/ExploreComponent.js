5fbdbea3324b6e7bb3f6d9656cda5e73d4c3ef4d
jannunzi <jannunzi@gmail.com>
2021-10-06 23:20:04 -0400

    replace class with className

--- src/components/ExploreScreen/ExploreComponent.js
+++ src/components/ExploreScreen/ExploreComponent.js
@@ -4,48 +4,48 @@ import PostSummaryList from "../PostSummaryList";
 const ExploreComponent = () => {
     return(
         <>
-            <div class="row">
-                <div class="col-11" style="position: relative">
-                    <i class="fa fa-search position-absolute"
+            <div className="row">
+                <div className="col-11" style="position: relative">
+                    <i className="fa fa-search position-absolute"
                        style="left: 25px; top: 11px"></i>
-                    <input placeholder="Search Twitter" class="ps-5 form-control rounded-pill mb-2 col-11"/>
+                    <input placeholder="Search Twitter" className="ps-5 form-control rounded-pill mb-2 col-11"/>
                 </div>
-                <div class="col-1">
+                <div className="col-1">
                     <a href="setting.html">
-                        <i class="mt-1 fa fa-2x fa-cog float-end"></i>
+                        <i className="mt-1 fa fa-2x fa-cog float-end"></i>
                     </a>
                 </div>
             </div>
-            <ul class="nav mb-2 nav-tabs">
-                <li class="nav-item">
-                    <a class="nav-link active" href="for-you.html">
+            <ul className="nav mb-2 nav-tabs">
+                <li className="nav-item">
+                    <a className="nav-link active" href="for-you.html">
                         For you
                     </a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="trending.html">
+                <li className="nav-item">
+                    <a className="nav-link" href="trending.html">
                         Trending
                     </a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="news.html">
+                <li className="nav-item">
+                    <a className="nav-link" href="news.html">
                         News
                     </a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="sports.html">
+                <li className="nav-item">
+                    <a className="nav-link" href="sports.html">
                         Sports
                     </a>
                 </li>
-                <li class="nav-item d-sm-none d-md-inline">
-                    <a class="nav-link" href="entertainment.html">
+                <li className="nav-item d-sm-none d-md-inline">
+                    <a className="nav-link" href="entertainment.html">
                         Entertainment
                     </a>
                 </li>
             </ul>
-            <div class="position-relative mb-2">
+            <div className="position-relative mb-2">
                 <img src="../../../images/Starship-Mk1-Day.jpg" width="100%"/>
-                <h2 class="fw-bold ms-2 text-light position-absolute bottom-0">SpaceX's Starship</h2>
+                <h2 className="fw-bold ms-2 text-light position-absolute bottom-0">SpaceX's Starship</h2>
             </div>
             ${PostSummaryList()}
         </>
