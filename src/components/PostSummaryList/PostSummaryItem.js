7013286e37dd7de15f809537c05fbf939d96d77f
jannunzi <jannunzi@gmail.com>
2021-10-06 23:06:21 -0400

    convert parameter to object destructor provide default values

--- src/components/PostSummaryList/PostSummaryItem.js
+++ src/components/PostSummaryList/PostSummaryItem.js
@@ -1,6 +1,15 @@
 import React from "react";
 
-const PostSummaryItem = (post) => {
+const PostSummaryItem = (
+    {
+        post = {
+            "topic": "Web Development",
+            "userName": "ReactJS",
+            "time": "2h",
+            "title": "React.js is a component based front end library that makes it very easy to build Single Page Applications or SPAs",
+            "image": "../../../images/react-blue.png"
+        }
+    }) => {
     return(
         <li className="list-group-item">
             <div className="row">
