1a7d1d75f908733bb076c6f8930986966333acdd
jannunzi <jannunzi@gmail.com>
2021-10-06 22:30:03 -0400

    copy from public folder

---
+++ src/components/WhoToFollowList/index.js
@@ -0,0 +1,19 @@
+import WhoToFollowListItem from "./WhoToFollowListItem.js";
+import who from "./who.js"
+
+const WhoToFollowList = () => {
+    return (`
+        <ul class="list-group">
+            <li class="list-group-item fw-bold">
+                Who to follow
+            </li>
+        ${who.map(who => {
+            return(
+                WhoToFollowListItem(who)
+            );
+        }).join('')}
+        </ul>
+    `);
+}
+
+export default WhoToFollowList;
