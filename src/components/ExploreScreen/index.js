954222632e4596b8b30f420680a0bb43a50ac96f
jannunzi <jannunzi@gmail.com>
2021-10-06 23:27:13 -0400

    replace IIFE and jquery with ExploreScreen function replace class with className

--- src/components/ExploreScreen/index.js
+++ src/components/ExploreScreen/index.js
@@ -3,18 +3,19 @@ import NavigationSidebar from "../NavigationSidebar";
 import ExploreComponent from "./ExploreComponent.js";
 import WhoToFollowList from "../WhoToFollowList";
 
-(function ($) {
-    $('#wd-explore').append(`
-        <div class="row mt-2">
-            <div class="col-2 col-md-2 col-lg-1 col-xl-2">
+const ExploreScreen = () => {
+    return(
+        <div className="row mt-2">
+            <div className="col-2 col-md-2 col-lg-1 col-xl-2">
                 ${NavigationSidebar('explore')}
             </div>
-            <div class="col-10 col-md-10 col-lg-7 col-xl-6" style="position: relative">
+            <div className="col-10 col-md-10 col-lg-7 col-xl-6"
+                 style="position: relative">
                 ${ExploreComponent()}
             </div>
-            <div class="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+            <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
                 ${WhoToFollowList()}
             </div>
         </div>
-    `);
-})($);
+    )
+}
