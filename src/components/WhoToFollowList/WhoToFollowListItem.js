9998be87f90b25031b9933545e3a4bad925f057b
jannunzi <jannunzi@gmail.com>
2021-10-06 22:19:39 -0400

    remove $ since these are not strings anymore

--- src/components/WhoToFollowList/WhoToFollowListItem.js
+++ src/components/WhoToFollowList/WhoToFollowListItem.js
@@ -5,16 +5,16 @@ const WhoToFollowListItem = (who) => {
         <li className="list-group-item">
             <div className="row">
                 <div className="col-2">
-                    <img src="${who.avatarIcon}" width="48" className="rounded-circle float-start"/>
+                    <img src={who.avatarIcon} width="48" className="rounded-circle float-start"/>
                 </div>
                 <div className="col-8">
-                    <div className="fw-bold">${who.userName}
+                    <div className="fw-bold">{who.userName}
                         <span className="fa-stack" style="font-size: 0.5em">
                           <i className="fas fa-circle fa-stack-2x"></i>
                           <i className="fas fa-check fa-stack-1x fa-inverse"></i>
                         </span>
                     </div>
-                    <div>@${who.handle}</div>
+                    <div>@{who.handle}</div>
                 </div>
                 <div className="col-2">
                     <button className="rounded-pill float-end btn btn-primary mt-1">Follow</button>
