cdb967143c84de96544f07ce9843457481fb4dd5
jannunzi <jannunzi@gmail.com>
2021-10-06 23:25:32 -0400

    import React

--- src/components/ExploreScreen/index.js
+++ src/components/ExploreScreen/index.js
@@ -1,3 +1,4 @@
+import React from "react";
 import NavigationSidebar from "../NavigationSidebar";
 import ExploreComponent from "./ExploreComponent.js";
 import WhoToFollowList from "../WhoToFollowList";
