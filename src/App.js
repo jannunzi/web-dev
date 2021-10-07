a69c7c58aedc834187bfcdf2716036577df45126
jannunzi <jannunzi@gmail.com>
2021-10-06 22:14:17 -0400

    port over NavigationSidebar to a React component display in root App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './vendors/bootstrap/css/bootstrap.min.css';
 import './vendors/fontawesome/css/all.min.css';
+import NavigationSidebar from "./components/NavigationSidebar/index";
 
 function App() {
   return (
     <div className="container">
-      <h1>Hello World</h1>
+      <NavigationSidebar/>
     </div>
   );
 }
