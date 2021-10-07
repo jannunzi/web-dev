b3469f950917cc39ca049ebfe0ebfd1b748118a2
jannunzi <jannunzi@gmail.com>
2021-10-06 23:09:08 -0400

    start migrating PostSummaryList from JavaScript to React and JSX

---
+++ src/components/PostSummaryList/index.js
@@ -0,0 +1,15 @@
+import PostSummaryItem from "./PostSummaryItem.js";
+import posts from "./posts.js";
+
+const PostSummaryList = () => {
+    return(`
+        <ul class="list-group">
+            ${
+                posts.map(post => {
+                    return (PostSummaryItem(post));
+                }).join('')
+            }
+        </ul>
+    `);
+}
+export default PostSummaryList;
