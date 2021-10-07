29286ab71ee85783e0134ffce409b5a82afc94be
jannunzi <jannunzi@gmail.com>
2021-10-06 22:23:40 -0400

    import WhoToFollowListItem

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import logo from './logo.svg';
 import './vendors/bootstrap/css/bootstrap.min.css';
 import './vendors/fontawesome/css/all.min.css';
 import NavigationSidebar from "./components/NavigationSidebar/index";
+import WhoToFollowListItem
+    from "./components/WhoToFollowList/WhoToFollowListItem";
 
 function App() {
   return (
     <div className="container">
-      <NavigationSidebar/>
+        <NavigationSidebar/>
+        <WhoToFollowListItem/>
     </div>
   );
 }
