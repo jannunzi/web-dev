d236847137839882d1906b5d2e1df9a5183b749d
jannunzi <jannunzi@gmail.com>
2021-10-06 22:38:20 -0400

    import WhoToFollowList

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './vendors/fontawesome/css/all.min.css';
 import NavigationSidebar from "./components/NavigationSidebar/index";
 import WhoToFollowListItem
     from "./components/WhoToFollowList/WhoToFollowListItem";
+import WhoToFollowList from "./components/WhoToFollowList";
 
 function App() {
   return (
     <div className="container">
         <NavigationSidebar active="home"/>
+        <WhoToFollowList/>
         <WhoToFollowListItem who={{
             avatarIcon: '../../../images/virgin.png',
             userName: 'Virgin Galactic',
