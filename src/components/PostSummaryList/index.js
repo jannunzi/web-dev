a4696fe0898e6f56fcecc8d5d9d90eecfb941a4e
jannunzi <jannunzi@gmail.com>
2021-10-06 23:13:58 -0400

    remove unnecessary .join()

--- src/components/PostSummaryList/index.js
+++ src/components/PostSummaryList/index.js
@@ -10,7 +10,7 @@ const PostSummaryList = () => {
                     return (
                         <PostSummaryItem post={post}/>
                     );
-                }).join('')
+                })
             }
         </ul>
     );
