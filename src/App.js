4e33ea0ffd9a8acaaa3f90a2207c800d60cb395e
jannunzi <jannunzi@gmail.com>
2021-10-06 23:07:06 -0400

    import it in App.js to test

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import NavigationSidebar from "./components/NavigationSidebar/index";
 import WhoToFollowListItem
     from "./components/WhoToFollowList/WhoToFollowListItem";
 import WhoToFollowList from "./components/WhoToFollowList";
+import PostSummaryItem from "./components/PostSummaryList/PostSummaryItem";
 
 function App() {
   return (
     <div className="container">
         <NavigationSidebar active="home"/>
         <WhoToFollowList/>
+        <PostSummaryItem/>
     </div>
   );
 }
