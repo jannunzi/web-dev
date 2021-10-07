c75caba1418d00016da9c5316f3f5adf943b1209
jannunzi <jannunzi@gmail.com>
2021-10-06 23:28:30 -0400

    replace NavigationSidebar function call with JSX tag

--- src/components/ExploreScreen/index.js
+++ src/components/ExploreScreen/index.js
@@ -7,7 +7,7 @@ const ExploreScreen = () => {
     return(
         <div className="row mt-2">
             <div className="col-2 col-md-2 col-lg-1 col-xl-2">
-                ${NavigationSidebar('explore')}
+                <NavigationSidebar active="explore"/>
             </div>
             <div className="col-10 col-md-10 col-lg-7 col-xl-6"
                  style="position: relative">
