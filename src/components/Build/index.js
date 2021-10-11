3d76d4642194d8c44e44a39c94504109737fc445
jannunzi <jannunzi@gmail.com>
2021-10-10 20:41:48 -0400

    latest

---
+++ src/components/Build/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import NavigationSidebar from "./NavigationSidebar";
+const Build = () => {
+    return(
+        <NavigationSidebar active="home"/>
+    )
+};
+export default Build;
