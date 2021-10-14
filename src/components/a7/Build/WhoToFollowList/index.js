7c822bb72ae5e385789d2eca4880e342d7fae4b9
jannunzi <jannunzi@gmail.com>
2021-10-14 09:21:48 -0400

    add who selector function

--- src/components/a7/Build/WhoToFollowList/index.js
+++ src/components/a7/Build/WhoToFollowList/index.js
@@ -2,8 +2,10 @@ import React from "react";
 import WhoToFollowListItem from "./WhoToFollowListItem";
 import {useSelector} from "react-redux";
 
+const selectAllWho = (state) => state.who.who;
+
 const WhoToFollowList = () => {
-    const who = useSelector((state) => state.who.who);
+    const who = useSelector(selectAllWho);
     return (
         <ul className="list-group">
             <li className="list-group-item fw-bold">
