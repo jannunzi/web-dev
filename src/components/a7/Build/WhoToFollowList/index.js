eeade81cb69c6f26abeafdf97ffe293d5f9d8229
jannunzi <jannunzi@gmail.com>
2021-10-14 09:20:27 -0400

    retrieve who from state

--- src/components/a7/Build/WhoToFollowList/index.js
+++ src/components/a7/Build/WhoToFollowList/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import WhoToFollowListItem from "./WhoToFollowListItem";
-import who from "./who.json"
+import {useSelector} from "react-redux";
 
 const WhoToFollowList = () => {
+    const who = useSelector((state) => state.who.who);
     return (
         <ul className="list-group">
             <li className="list-group-item fw-bold">
