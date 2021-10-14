39d9d7c9554b377793441d0c7e52fc8f2cca8fb3
jannunzi <jannunzi@gmail.com>
2021-10-13 23:39:49 -0400

    create posts reducer

---
+++ src/reducers/posts.js
@@ -0,0 +1,11 @@
+import postsJson from './data/posts.json';
+
+const initialState = {
+    posts: postsJson
+}
+
+const posts = (state = initialState) => {
+    return(state);
+};
+
+export default posts;
