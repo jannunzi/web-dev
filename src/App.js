2889f63a73c1360478bf11af9c8c7f8c4773cadc
jannunzi <jannunzi@gmail.com>
2021-10-13 23:40:18 -0400

    create store and provider

--- src/App.js
+++ src/App.js
@@ -2,21 +2,28 @@ import './vendors/bootstrap/css/bootstrap.min.css';
 import './vendors/bootstrap/bootstrap.min.css';
 import './vendors/fontawesome/css/all.min.css';
 import {BrowserRouter, Route} from "react-router-dom";
-import HelloWorld from "./components/a6/HelloWorld";
 import Practice from "./components/a6/Practice";
 import HomeScreen from "./components/a7/Build/HomeScreen";
 import ExploreScreen from "./components/a7/Build/ExploreScreen";
+import posts from "./reducers/posts";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+
+const store = createStore(posts);
+
 function App() {
   return (
-      <BrowserRouter>
-          <div className="container">
-              <Route path={["/a7", "/a7/practice"]} exact={true}>
-                  <Practice/>
-              </Route>
-              <Route path={["/", "/a7/twitter/home"]} exact={true} component={HomeScreen}/>
-              <Route path="/a7/twitter/explore" exact={true} component={ExploreScreen}/>
-          </div>
-      </BrowserRouter>
+      <Provider store={store}>
+          <BrowserRouter>
+              <div className="container">
+                  <Route path={["/a7", "/a7/practice"]} exact={true}>
+                      <Practice/>
+                  </Route>
+                  <Route path={["/", "/a7/twitter/home"]} exact={true} component={HomeScreen}/>
+                  <Route path="/a7/twitter/explore" exact={true} component={ExploreScreen}/>
+              </div>
+          </BrowserRouter>
+      </Provider>
   );
 }
 
