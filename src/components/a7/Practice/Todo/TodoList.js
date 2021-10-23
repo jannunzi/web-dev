6e17f4a79c4fac93539c1b1381f8a4c8b56efb6c
jannunzi <jannunzi@gmail.com>
2021-10-23 09:03:20 -0400

    copy Practice examples

---
+++ src/components/a7/Practice/Todo/TodoList.js
@@ -0,0 +1,16 @@
+import TodoItem from "./TodoItem";
+import todos from "./todos.json";
+const TodoList = () => {
+    return(
+        <ul>
+            {
+                todos.map(todo => {
+                    return(
+                        <TodoItem todo={todo}/>
+                    );
+                })
+            }
+        </ul>
+    );
+}
+export default TodoList;
---
+++ src/components/a7/Practice/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Classes from "./Classes";
+import Styles from "./Styles";
+import ConditionalOutput from "./ConditionalOutput";
+import TodoItem from "./Todo/TodoItem";
+import TodoList from "./Todo/TodoList";
+import {Link} from "react-router-dom";
+import ReduxExamples from "./ReduxExamples/components";
+const Practice = () => {
+    return(
+        <div>
+            <h1>Practice</h1>
+            <Link to="/a7/hello">Hello</Link> |
+            <Link to="/a7/twitter/home">Build</Link>
+            <ReduxExamples/>
+            <TodoList/>
+            <TodoItem/>
+            <ConditionalOutput/>
+            <Styles/>
+            <Classes/>
+        </div>
+    )
+};
+
+export default Practice;
