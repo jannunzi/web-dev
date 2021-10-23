b95f6a95d629854d774868829ff7267cf00a6490
jannunzi <jannunzi@gmail.com>
2021-10-23 09:45:39 -0400

    create todos reducer

---
+++ src/components/a7/Practice/ReduxExamples/reducers/todos.js
@@ -0,0 +1,14 @@
+const data = [
+    {
+        do: "Accelerate the world's transition to sustainable energy"
+    },
+    {
+        do: "Reduce space transportation costs to become a spacefaring civilization"
+    },
+];
+
+const todos = () => {
+    return data;
+}
+
+export default todos;
