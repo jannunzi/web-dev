052d694b9050901a3a2f72fc2cd18b40fcb28647
jannunzi <jannunzi@gmail.com>
2021-10-22 23:05:01 -0400

    like a tweet

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -6,6 +6,24 @@ const initialState = {
 
 const tweets = (state = initialState, action) => {
     switch (action.type) {
+        case 'like-tweet':
+            return ({
+                tweets: state.tweets.map(tweet => {
+                    if(tweet._id === action.tweet._id) {
+                        if(tweet.liked === true) {
+                            tweet.liked = false;
+                            tweet.stats.likes--;
+                        } else {
+                            tweet.liked = true;
+                            tweet.stats.likes++;
+                        }
+                        return tweet;
+                    } else {
+                        return tweet;
+                    }
+                })
+            });
+            break;
         case 'delete-tweet':
             return ({
                 tweets: state.tweets.filter(tweet => tweet._id !== action.tweet._id)
