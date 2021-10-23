103af73b80b20baf319ed18353622049693fe091
jannunzi <jannunzi@gmail.com>
2021-10-23 09:54:01 -0400

    hello

--- src/components/a7/Practice/ReduxExamples/components/HelloReduxExampleComponent.js
+++ src/components/a7/Practice/ReduxExamples/components/HelloReduxExampleComponent.js
@@ -2,7 +2,7 @@ import React from "react";
 import {useSelector} from "react-redux";
 
 const HelloReduxExampleComponent = () => {
-    const message = useSelector((state) => state.message);
+    const message = useSelector((state) => state.hello.message);
     return(
         <h1>{message}</h1>
     );
