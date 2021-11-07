08622ebd82e59471b92ff3ec16f75add775f2947
JoseAnnunziato <jose.annunziato@sonos.com>
2021-11-07 09:40:08 -0500

    create movie

--- src/components/a9/Practice/Movies/index.js
+++ src/components/a9/Practice/Movies/index.js
@@ -6,12 +6,23 @@ const Movies = () => {
 useEffect(() =>
   service.findAllMovies()
     .then(movies => setMovies(movies)), []);
-const deleteMovie = (movie) =>
-  service.deleteMovie(movie._id)
-    .then(() => setMovies(
-      movies.filter(m => m !== movie)));
+  const deleteMovie = (movie) =>
+    service.deleteMovie(movie._id)
+      .then(() => setMovies(
+        movies.filter(m => m !== movie)));
+  const createMovie = () =>
+    service.createMovie(({title: 'New Movie'}))
+      .then((actualMovie) =>
+        setMovies([
+          actualMovie, ...movies
+      ]));
   return(
     <div>
+      <button
+        onClick={createMovie}
+        className="btn btn-success float-end">
+        Create
+      </button>
       <h2>Movies</h2>
       <ul className="list-group">
         {
